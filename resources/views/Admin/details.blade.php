21f20564cd14e30596ca960f81334691b1de7283
Aymanoox <aymangamer02@gmail.com>
2023-04-09 14:16:28 +0000

    update

--- resources/views/Admin/details.blade.php
+++ resources/views/Admin/details.blade.php
@@ -29,27 +29,6 @@
 </div>
 @endforeach
 
-<div class="card">
-    <div class="card-header">Products</div>
-    <div class="card-body">
-        <table class="table table-bordered mb-0">
-            <thead>
-                <tr>
-                    <th>nom produit</th>
-                    <th>prix</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($comande as $c)
-                <tr>
-                    <td>{{$c->name_pro}}</td>
-                    <td>{{$c->prix_pro}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-</div>
 
 
             <div class="card">
